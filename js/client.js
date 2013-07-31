f9d6a8ded320054c5d21e5382e4c45ce15d39f10
cScarlson <otocarlson@gmail.com>
2013-07-31 02:07:06 -0600

    Solved duplicate emission issue(s)

--- js/client.js
+++ js/client.js
@@ -1,11 +1,20 @@
 
 $bee({path: '/js/sharedWorker.js', ns: 'NDN'}).ready(function(bee, port, sw){
   
-  bee.on('custom', function(data, e){
-    alert('Really, ' + data);
+  bee.on('custom', function(e){
+    console.log('Really,', e.data);
   });
   
   bee.emit('uncustom', 'I DON\'t like turtles...!');
+  
+  bee.on('broadcast', function(e){
+    alert('New Broadcast Message: ' + e.data);
+  });
+  
+  document.querySelector('button').addEventListener('click', function(){
+    var msg = document.querySelector('textarea').value;
+    bee.emit('broadcast', msg);
+  }, false);
   
 });
 
--- js/sharedWorker.js
+++ js/sharedWorker.js
@@ -1,9 +1,15 @@
 self.importScripts('/lib/scriptorium.js');
 
-$hive(function(hive, port, ports, e){
+$hive(function(hive, port, ports, event){
+
   hive.emit('custom', 'I like turtles...');
-  hive.on('uncustom', function(data){
+  hive.on('uncustom', function(e){
     hive.emit('custom', 'You LOVE turtles...');
+  });
+  
+  hive.on('broadcast', function(e){
+    port.postMessage('???');
+    //hive.emit('broadcast', e.data);
   });
   
 }).load('/lib/fakeLib.js');
